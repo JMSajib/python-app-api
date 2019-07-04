281a422c4dfeda6b841adc7fbf2560323f11dc28
JMSajib <jm.sajib012@gmail.com>
2019-07-04 12:39:05 +0600

    Email Normalized and validation Rule set

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,9 @@ class UserManager(BaseUserManager):
     
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an Email Address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
