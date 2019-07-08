e14f539f5222ec16667be8ccf511df7ee014b2ad
JMSajib <jm.sajib012@gmail.com>
2019-07-08 20:07:41 +0600

    Added AdiminsiteTest

--- app/core/models.py
+++ app/core/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import AbstractBaseUser, \
                                         BaseUserManager, \
                                         PermissionsMixin
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
-            raise ValueError("User must have an Email Address")
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
