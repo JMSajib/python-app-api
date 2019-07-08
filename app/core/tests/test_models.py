c9fb606b5e175aa3fead69473eba0d61c8db3593
JMSajib <jm.sajib012@gmail.com>
2019-07-08 17:58:14 +0600

    Added super user script

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,4 +26,13 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')    
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com', 'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
