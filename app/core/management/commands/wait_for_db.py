87400c7aea21e1237ea2cabd91c04e89dade6f96
JMSajib <jmsajibcse@gmail.com>
2019-07-23 21:10:50 +0600

    configure database for test

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,22 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        """Django command to pause execution until database is available"""
+        self.stdout.write('Waiting for Databse...')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database unavilable,waiting for 1 sec...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database avilable!!'))
+     
